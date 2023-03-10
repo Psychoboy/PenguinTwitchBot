53cf7b2c35854447ac13e61cce0d58e9815f5d8f
SuperPenguinTV <psycoboy@gmail.com>
2023-03-09 21:57:32 -0700

    Added raffles

---
+++ DotNetTwitchBot/Bot/Commands/Games/PancakeRaffle.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetTwitchBot.Bot.Commands.Features;
+using DotNetTwitchBot.Bot.Core;
+using DotNetTwitchBot.Bot.Events;
+
+namespace DotNetTwitchBot.Bot.Commands.Games
+{
+    public class PancakeRaffle : BaseRaffle
+    {
+        public PancakeRaffle(
+            ServiceBackbone eventService,
+            PointsFeature pointsFeature
+        ) : base(eventService, pointsFeature, "sptvPancake", "!pancake", "pancake")
+        {
+        }
+
+        protected override async Task OnCommand(object? sender, CommandEventArgs e)
+        {
+            switch (e.Command) {
+                case "testpancakestart": {
+                    if(e.Args.Count == 0) return;
+                    if(!_eventService.IsBroadcasterOrBot(e.Sender)) return;
+                    if(Int32.TryParse(e.Args[0], out int amount)) {
+                        await StartRaffle(e.Sender, amount);
+                    }
+                }
+                break;
+
+                case "pancake": {
+                    await EnterRaffle(e.Sender);
+                }
+                break;
+            }
+        }
+    }
+}
