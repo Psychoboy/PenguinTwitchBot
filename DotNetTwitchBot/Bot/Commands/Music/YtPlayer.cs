51f03f6b03066e071ca4406f51e6508bcab2fca9
SuperPenguinTV <psycoboy@gmail.com>
2023-03-22 16:29:54 -0700

    Fixed issues and basics of Music Player

---
+++ DotNetTwitchBot/Bot/Commands/Music/YtPlayer.cs
@@ -0,0 +1,179 @@
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetTwitchBot.Bot.Core;
+using DotNetTwitchBot.Bot.Events;
+using Microsoft.AspNetCore.SignalR;
+using Google.Apis.YouTube.v3;
+
+namespace DotNetTwitchBot.Bot.Commands.Music
+{
+    public class YtPlayer : BaseCommand
+    {
+        private IHubContext<YtHub> _hubContext;
+        private YouTubeService _youtubeService;
+        private List<Song> Requests = new List<Song>();
+        private List<Song> BackupSongs = new List<Song>();
+        private PlayerState State = PlayerState.UnStarted;
+        enum PlayerState
+        {
+            UnStarted = -1,
+            Ended = 0,
+            Playing = 1,
+            Paused = 2,
+            Buffer = 3,
+            VideoCued = 5
+        }
+
+        public YtPlayer(
+            IConfiguration configuration,
+            IHubContext<YtHub> hubContext,
+            ServiceBackbone serviceBackbone
+        ) : base(serviceBackbone)
+        {
+            _hubContext = hubContext;
+            // _configuration = configuration;
+            // ApiKey = _configuration["youtubeApi"];
+            _youtubeService = new YouTubeService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApiKey = configuration["youtubeApi"],
+                ApplicationName = "DotNetBot"
+            });
+        }
+
+        public async Task<string> GetNextSong()
+        {
+            if (Requests.Count > 0)
+            {
+                var song = Requests.First();
+                Requests.RemoveAt(0);
+                return song.SongId;
+            }
+            if (BackupSongs.Count == 0)
+            {
+                await LoadBackupList();
+            }
+            var randomSong = BackupSongs[Tools.CurrentThreadRandom.Next(BackupSongs.Count)].SongId;
+            return randomSong;
+        }
+
+        private async Task LoadBackupList()
+        {
+            var song = await GetSong("ZyhrYis509A");
+            if (song != null)
+                BackupSongs.Add(song);
+            song = await GetSong("EAwWPadFsOA");
+            if (song != null)
+                BackupSongs.Add(song);
+        }
+
+        public async void UpdateState(int state)
+        {
+            this.State = (PlayerState)state;
+            if (this.State == PlayerState.Ended)
+            {
+                await PlayNextSong();
+            }
+        }
+
+        public async Task PlayNextSong()
+        {
+            await _hubContext.Clients.All.SendAsync("PlayVideo", await GetNextSong());
+        }
+
+        public async Task Pause()
+        {
+            if (State == PlayerState.Playing)
+            {
+                await _hubContext.Clients.All.SendAsync("Pause");
+            }
+            else
+            {
+                await _hubContext.Clients.All.SendAsync("Play");
+            }
+        }
+
+        public async Task LoadNextSong()
+        {
+            await _hubContext.Clients.All.SendAsync("PlayVideo", await GetNextSong());
+        }
+
+        protected override async Task OnCommand(object? sender, CommandEventArgs e)
+        {
+            switch (e.Command)
+            {
+                case "testnextsong":
+                    await PlayNextSong();
+                    break;
+                case "testpause":
+                    await Pause();
+                    break;
+                case "testsr":
+                    await SongRequest(e);
+                    break;
+            }
+        }
+
+        private async Task SongRequest(CommandEventArgs e)
+        {
+            if (Requests.Where(x => x.RequestedBy.Equals(e.DisplayName)).Count() >= 30)
+            {
+                await _serviceBackbone.SendChatMessage(e.DisplayName, "You already have your quota(30) of songs in the queue.");
+                return;
+            }
+            var client = new HttpClient();
+            var httpRequest = new HttpRequestMessage()
+            {
+                RequestUri = new Uri(string.Format("https://beta.decapi.me/youtube/videoid?search={0}", e.Arg)),
+                Method = HttpMethod.Get
+            };
+            var searchResponse = await client.SendAsync(httpRequest);
+            var searchResult = await searchResponse.Content.ReadAsStringAsync();
+            var song = await GetSong(searchResult);
+            if (song == null)
+            {
+                await _serviceBackbone.SendChatMessage(e.DisplayName, string.Format("Could not get or issue finding your song request"));
+                return;
+            }
+            if (song.Duration > new TimeSpan(0, 10, 0))
+            {
+                await _serviceBackbone.SendChatMessage(e.DisplayName, string.Format("Your song is to long. Max is 10 minutes and yours is: {0:c}", song.Duration));
+                return;
+            }
+
+            song.RequestedBy = e.DisplayName;
+            Requests.Add(song);
+
+            await _serviceBackbone.SendChatMessage(e.DisplayName, string.Format("{0} was added to the song queue in position #{1}", song.Title, Requests.Count));
+        }
+
+        private async Task<Song?> GetSong(string youtubeId)
+        {
+            var ytRequest = _youtubeService.Videos.List("snippet,contentDetails");
+            ytRequest.Id = youtubeId;
+
+            var ytResponse = await ytRequest.ExecuteAsync();
+            if (ytResponse != null && ytResponse.Items.Count > 0)
+            {
+                var item = ytResponse.Items.First();
+                TimeSpan length = new TimeSpan();
+                if (Iso8601DurationHelper.Duration.TryParse(item.ContentDetails.Duration, out var duration))
+                {
+                    length = new TimeSpan((int)duration.Hours, (int)duration.Minutes, (int)duration.Seconds);
+                }
+                return new Song
+                {
+                    Title = item.Snippet.Title,
+                    Duration = length,
+                    SongId = youtubeId
+                };
+            }
+            return null;
+        }
+
+
+    }
+}
