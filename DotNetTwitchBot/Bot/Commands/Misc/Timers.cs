2da16b6f2737d790cda9b3018c458dd7f32416f2
SuperPenguinTV <psycoboy@gmail.com>
2023-04-16 19:44:43 -0700

    More efficient timer message

--- DotNetTwitchBot/Bot/Commands/Misc/Timers.cs
+++ DotNetTwitchBot/Bot/Commands/Misc/Timers.cs
@@ -133,7 +133,7 @@ namespace DotNetTwitchBot.Bot.Commands.Misc
             try
             {
                 if (group.Messages.Where(x => x.Enabled == true).Any() == false) return;
-                var message = group.Messages.Where(x => x.Enabled == true).ToList().RandomElement();
+                var message = group.Messages.Where(x => x.Enabled == true).RandomElement();
                 await SendMessage(message);
 
             }
