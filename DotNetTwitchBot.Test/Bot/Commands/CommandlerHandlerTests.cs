cb4dc3f4b6527fa7cecb1fac932dd5e103b70058
SuperPenguinTV <psycoboy@gmail.com>
2023-08-15 07:14:42 -0700

    more test coverage YAY

--- DotNetTwitchBot.Test/Bot/Commands/CommandlerHandlerTests.cs
+++ DotNetTwitchBot.Test/Bot/Commands/CommandlerHandlerTests.cs
@@ -8,6 +8,7 @@ using DotNetTwitchBot.Bot.Commands;
 using DotNetTwitchBot.Bot.Core;
 using DotNetTwitchBot.Bot.Models;
 using DotNetTwitchBot.Bot.Repository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MockQueryable.NSubstitute;
@@ -56,7 +57,7 @@ namespace DotNetTwitchBot.Tests.Bot.Commands
         }
 
         [Fact]
-        public void AddCommand_Should_AddCommand_WithCorrectProperties()
+        public void AddCommand_Should_AddCommand_WithDefaultProperties()
         {
             // Arrange
             var logger = Substitute.For<ILogger<CommandHandler>>();
@@ -68,6 +69,26 @@ namespace DotNetTwitchBot.Tests.Bot.Commands
 
             // Act
             commandHandler.AddCommand(new DefaultCommand {CommandName = "testCommand", CustomCommandName = "testCommand" }, commandService);
+
+            // Assert
+            var result = commandHandler.GetCommand("testCommand");
+            Assert.NotNull(result);
+            Assert.Equal("testCommand", result.CommandProperties.CommandName);
+        }
+
+        [Fact]
+        public void AddCommand_Should_AddCommand_WithNoneDefaultProperties()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var commandService = Substitute.For<IBaseCommandService>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+
+            // Act
+            commandHandler.AddCommand(new BaseCommandProperties { CommandName = "testCommand" }, commandService);
 
             // Assert
             var result = commandHandler.GetCommand("testCommand");
@@ -150,7 +171,275 @@ namespace DotNetTwitchBot.Tests.Bot.Commands
             dbContext.Received(1).Update(defaultCommand);
         }
 
-        // more tests for the remaining methods
+
+        [Fact]
+        public async Task UpdateDefaultCommand_ShouldNot_UpdateDefaultCommand_NoId()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { CustomCommandName = "newCommand", CommandName = "testCommand" };
+            var testCommand = new DefaultCommand { Id = 1, CommandName = "testCommand", CustomCommandName = "testCommand" };
+            commandHandler.AddCommand(testCommand, commandService);
+            var queryable = new List<DefaultCommand> { testCommand }.AsQueryable().BuildMockDbSet();
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+
+
+
+            // Act
+            await commandHandler.UpdateDefaultCommand(defaultCommand);
+
+            // Assert
+            dbContext.Received(0).Update(defaultCommand);
+        }
+
+
+        [Fact]
+        public async Task UpdateDefaultCommand_ShouldNot_UpdateDefaultCommand_NotAdded()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { Id = 1, CustomCommandName = "newCommand", CommandName = "testCommand" };
+           
+            var queryable = new List<DefaultCommand> {  }.AsQueryable().BuildMockDbSet();
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+
+            // Act
+            await commandHandler.UpdateDefaultCommand(defaultCommand);
+
+            // Assert
+            dbContext.Received(0).Update(defaultCommand);
+        }
+
+        [Fact]
+        public async Task GetDefaultCommandFromDb_ShouldReturnCorrectCommand()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { Id = 1, CustomCommandName = "testCommand", CommandName = "testCommand" };
+
+            var queryable = new List<DefaultCommand> { defaultCommand }.AsQueryable().BuildMockDbSet();
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+
+            //defaultCommandRepository.Find(x => true).ReceivedWithAnyArgs(var queryable = new List<DefaultCommand> { testCommand }.AsQueryable().BuildMockDbSet();)
+            //    .FirstOrDefaultAsync().Returns(expectedCommand);
+
+            // Act
+            var result = await commandHandler.GetDefaultCommandFromDb("testCommand");
+
+            // Assert
+            Assert.Equal(defaultCommand, result);
+        }
+
+        [Fact]
+        public async Task GetDefaultCommandById_ShouldReturnCorrectCommand()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { Id = 1, CustomCommandName = "testCommand", CommandName = "testCommand" };
+
+            var queryable = new List<DefaultCommand> { defaultCommand }.AsQueryable().BuildMockDbSet();
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+
+            //defaultCommandRepository.Find(Arg.Any<Func<DefaultCommand, bool>>())
+            //    .FirstOrDefaultAsync().Returns(expectedCommand);
+
+            // Act
+            var result = await commandHandler.GetDefaultCommandById(1);
+
+            // Assert
+            Assert.Equal(defaultCommand, result);
+        }
+
+        [Fact]
+        public async Task GetDefaultCommandsFromDb_ShouldReturnListOfCommands()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { Id = 1, CustomCommandName = "testCommand", CommandName = "testCommand" };
+            var commands = new List<DefaultCommand>
+            {
+                new DefaultCommand { Id = 1, CommandName = "cmd1" },
+                new DefaultCommand { Id = 2, CommandName = "cmd2" }
+            };
+            var queryable = commands.AsQueryable().BuildMockDbSet();
+
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+            var expectedCommands =
+            dbContext.GetAllAsync().Returns(commands);
+
+            // Act
+            var result = await commandHandler.GetDefaultCommandsFromDb();
+
+            // Assert
+            Assert.Equal(commands, result);
+        }
+
+        [Fact]
+        public void IsCoolDownExpired_ShouldReturnTrue_WhenNoCooldowns()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var user = "testUser";
+            var command = "testCommand";
+
+            // Act
+            var result = commandHandler.IsCoolDownExpired(user, command);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsCoolDownExpired_ShouldReturnFalse_WhenGlobalCooldownActive()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var user = "testUser";
+            var command = "testCommand";
+            var globalCooldown = DateTime.Now.AddSeconds(10);
+            commandHandler.AddGlobalCooldown(command, globalCooldown);
+
+            // Act
+            var result = commandHandler.IsCoolDownExpired(user, command);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task IsCoolDownExpiredWithMessage_ShouldReturnFalse_WhenCooldownActive()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CommandHandler>>();
+            var scopeFactory = Substitute.For<IServiceScopeFactory>();
+
+            var dbContext = Substitute.For<IDefaultCommandRepository>();
+
+            var commandHandler = new CommandHandler(logger, scopeFactory);
+            var commandService = Substitute.For<IBaseCommandService>();
+
+
+            var defaultCommand = new DefaultCommand { Id = 1, CustomCommandName = "testCommand", CommandName = "testCommand" };
+
+            var queryable = new List<DefaultCommand> { defaultCommand }.AsQueryable().BuildMockDbSet();
+            dbContext.Find(x => true).ReturnsForAnyArgs(queryable);
+
+
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider.GetService(typeof(IDefaultCommandRepository)).Returns(dbContext);
+
+            var scope = Substitute.For<IServiceScope>();
+            scope.ServiceProvider.Returns(serviceProvider);
+
+            scopeFactory.CreateScope().Returns(scope);
+            var serviceBackbone = Substitute.For<IServiceBackbone>();
+            serviceProvider.GetService(typeof(IServiceBackbone)).Returns(serviceBackbone);
+
+            var displayName = "TestUser";
+
+            var user = "testUser";
+            var command = "testCommand";
+            var cooldown = DateTime.Now.AddSeconds(10);
+            commandHandler.AddCoolDown(user, command, cooldown);
+
+            // Act
+            var result = await commandHandler.IsCoolDownExpiredWithMessage(user, displayName, command);
+
+            // Assert
+            Assert.False(result);
+            await serviceBackbone.Received(1).SendChatMessage(displayName, Arg.Any<string>());
+        }
 
         private IQueryable<DefaultCommand> GetDefaultCommandDbSet()
         {
