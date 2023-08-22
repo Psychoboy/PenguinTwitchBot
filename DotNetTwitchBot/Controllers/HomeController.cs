1ba541ebd5328fd56983d3761800f6eb080be309
SuperPenguinTV <psycoboy@gmail.com>
2023-08-21 22:27:39 -0700

    Auth is mostly working now

--- DotNetTwitchBot/Controllers/HomeController.cs
+++ DotNetTwitchBot/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using DotNetTwitchBot.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace DotNetTwitchBot.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult Index()
         {
             return View();
@@ -25,6 +33,69 @@ namespace DotNetTwitchBot.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("/signin")]
+        public IActionResult Signin()
+        {
+            var url = getAuthorizationCodeUrl("https://localhost:7293/redirect", new());
+            return Redirect(url);
+        }
+
+        [HttpGet("/redirect")]
+        public async Task<IActionResult> RedirectFromTwitch([FromQuery(Name = "code")] string code)
+        {
+            var api = new TwitchLib.Api.TwitchAPI();
+            api.Settings.ClientId = _configuration["twitchClientId"];
+            var resp = await api.Auth.GetAccessTokenFromCodeAsync(code, _configuration["twitchClientSecret"], "https://localhost:7293/redirect");
+            api.Settings.AccessToken = resp.AccessToken;
+            var users = await api.Helix.Users.GetUsersAsync();
+            if (users.Users.Length > 0)
+            {
+                var user = users.Users[0];
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Login),
+                    new Claim(ClaimTypes.Role, _configuration["broadcaster"].Equals(user.Login) ? "Streamer": "Viewer")
+                };
+
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme
+                );
+
+                var authProperties = new AuthenticationProperties
+                {
+
+                };
+
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties
+                );
+            }
+            return Redirect("/");
+        }
+
+        [HttpGet("/signout")]
+        public async Task<IActionResult> Signout()
+        {
+            // Clear the existing external cookie
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+
+        private string getAuthorizationCodeUrl(string redirectUri, List<string> scopes)
+        {
+            var scopesStr = String.Join('+', scopes);
+
+            return "https://id.twitch.tv/oauth2/authorize?" +
+                   $"client_id={_configuration["twitchClientId"]}&" +
+                   $"redirect_uri={System.Web.HttpUtility.UrlEncode(redirectUri)}&" +
+                   "response_type=code&" +
+                   $"scope=";
         }
     }
 }
