34fb13b150485f62a3b281a82e1e15c445116d74
SuperPenguinTV <psycoboy@gmail.com>
2024-01-17 22:23:16 -0700

    Forgot to commit tests

--- DotNetTwitchBot.Test/Bot/Commands/CommandlerHandlerTests.cs
+++ DotNetTwitchBot.Test/Bot/Commands/CommandlerHandlerTests.cs
@@ -2,7 +2,7 @@
 using DotNetTwitchBot.Bot.Commands.Moderation;
 using DotNetTwitchBot.Bot.Core;
 using DotNetTwitchBot.Bot.Models;
-using DotNetTwitchBot.Bot.Repository;
+using DotNetTwitchBot.Repository;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MockQueryable.NSubstitute;
