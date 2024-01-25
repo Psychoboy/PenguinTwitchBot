ba90a001fae05c823699738074e727162f20cbc8
SuperPenguinTV <psycoboy@gmail.com>
2024-01-25 12:21:54 -0700

    Cleanup and safety

--- DotNetTwitchBot/Bot/Commands/TTS/TTSPlayerService.cs
+++ DotNetTwitchBot/Bot/Commands/TTS/TTSPlayerService.cs
@@ -6,7 +6,7 @@ using System.Speech.Synthesis;
 
 namespace DotNetTwitchBot.Bot.Commands.TTS
 {
-    public class TTSPlayerService : ITTSPlayerService
+    public class TTSPlayerService(ILogger<TTSPlayerService> logger) : ITTSPlayerService
     {
         public async Task PlayRequest(TTSRequest request)
         {
@@ -22,47 +22,62 @@ namespace DotNetTwitchBot.Bot.Commands.TTS
             }
         }
 
-        private static async Task PlayGoogle(TTSRequest request)
+        private async Task PlayGoogle(TTSRequest request)
         {
-            var credentials = GoogleCredential.FromFile("gtts.json");
-            var builder = new TextToSpeechClientBuilder
+            try
             {
-                Credential = credentials
-            };
-            var tts = await builder.BuildAsync();
-            var result = await tts.SynthesizeSpeechAsync(
-                new SynthesisInput { Text = request.Message },
-                new VoiceSelectionParams { LanguageCode = request.RegisteredVoice.LanguageCode, Name = request.RegisteredVoice.Name },
-                new AudioConfig { AudioEncoding = AudioEncoding.Mp3 }
-                );
-            var fileName = Guid.NewGuid().ToString();
-            using (var output = File.Create(fileName))
-            {
-                result.AudioContent.WriteTo(output);
-            }
+                var credentials = GoogleCredential.FromFile("gtts.json");
+                var builder = new TextToSpeechClientBuilder
+                {
+                    Credential = credentials
+                };
+                var tts = await builder.BuildAsync();
+                var result = await tts.SynthesizeSpeechAsync(
+                    new SynthesisInput { Text = request.Message },
+                    new VoiceSelectionParams { LanguageCode = request.RegisteredVoice.LanguageCode, Name = request.RegisteredVoice.Name },
+                    new AudioConfig { AudioEncoding = AudioEncoding.Mp3 }
+                    );
+                var fileName = Guid.NewGuid().ToString();
+                using (var output = File.Create(fileName))
+                {
+                    result.AudioContent.WriteTo(output);
+                }
 
-            using var mp3File = File.OpenRead(fileName);
-            using var waveOut = new WaveOutEvent();
-            using var mp3Reader = new Mp3FileReader(mp3File);
-            waveOut.Init(mp3Reader);
-            waveOut.Play();
-            while (waveOut.PlaybackState == PlaybackState.Playing)
-            {
-                Thread.Sleep(500);
-            }
+                using var mp3File = File.OpenRead(fileName);
+                using var waveOut = new WaveOutEvent();
+                using var mp3Reader = new Mp3FileReader(mp3File);
+                waveOut.Init(mp3Reader);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(500);
+                }
 
+                File.Delete(fileName);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to play google message.");
+            }
         }
 
-        private static void PlayWindows(TTSRequest request)
+        private void PlayWindows(TTSRequest request)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            try
             {
-                var synthesizer = new SpeechSynthesizer();
-                var builder = new PromptBuilder();
-                builder.StartVoice(request.RegisteredVoice.Name);
-                builder.AppendText(request.Message);
-                builder.EndVoice();
-                synthesizer.Speak(builder);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    var synthesizer = new SpeechSynthesizer();
+                    var builder = new PromptBuilder();
+                    builder.StartVoice(request.RegisteredVoice.Name);
+                    builder.AppendText(request.Message);
+                    builder.EndVoice();
+                    synthesizer.Speak(builder);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to play windows message.");
             }
         }
     }
