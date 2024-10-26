1afd53e942c677036b2934d33605032128485d30
SPTV <psycoboy@gmail.com>
2024-10-26 09:56:13 -0700

    Change TTS to just cleanup every hour

---
+++ DotNetTwitchBot/Bot/ScheduledJobs/HourlyCleanupJob.cs
@@ -0,0 +1,22 @@
+﻿using Quartz;
+
+namespace DotNetTwitchBot.Bot.ScheduledJobs
+{
+    public class HourlyCleanupJob(ILogger<HourlyCleanupJob> logger) : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            var files = Directory.GetFiles("wwwroot/tts/");
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.LastWriteTime < DateTime.Now.AddHours(-1))
+                {
+                    logger.LogInformation("Deleting TTS {file} Last Created {lastAccessed}", fileInfo.Name, fileInfo.LastWriteTime.ToString());
+                    fileInfo.Delete();
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
