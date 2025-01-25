5d5379919c164804ab80e74230f17210cf214707
SPTV <psycoboy@gmail.com>
2025-01-25 12:03:33 -0700

    Support weight for WheelSpin

---
+++ DotNetTwitchBot/Bot/Commands/WheelSpin/WheelSpinIndex.cs
@@ -0,0 +1,9 @@
+﻿using DotNetTwitchBot.Bot.Models.Wheel;
+
+namespace DotNetTwitchBot.Bot.Commands.WheelSpin
+{
+    public class WheelSpinIndex : WheelProperty
+    {
+        public int Index { get; set; }
+    }
+}
