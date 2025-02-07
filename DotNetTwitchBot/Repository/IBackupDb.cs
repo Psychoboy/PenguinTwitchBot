823752f1eea6b6e00e9a98ded4123558fc82c403
SPTV <psycoboy@gmail.com>
2025-02-06 21:01:30 -0700

    Refactor DB to make it easier to maintain

---
+++ DotNetTwitchBot/Repository/IBackupDb.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetTwitchBot.Repository
+{
+    public interface IBackupDb
+    {
+         Task BackupTable(DbContext context, string backupDirectory, ILogger? logger = null);
+        Task RestoreTable(DbContext context, string backupDirectory, ILogger? logger = null);
+    }
+}
--- DotNetTwitchBot/Repository/IGenericRepository.cs
+++ DotNetTwitchBot/Repository/IGenericRepository.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace DotNetTwitchBot.Repository
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IGenericRepository<T>: IBackupDb where T : class
     {
         T? GetById(int? id);
         Task<T?> GetByIdAsync(int id);
