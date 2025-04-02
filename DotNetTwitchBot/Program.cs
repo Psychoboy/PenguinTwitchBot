3193311d3dc645ceedc295ec592c429a367ebbea
Psychoboy <psycoboy@gmail.com>
2025-04-02 16:24:07 -0700

    Adds a cache to game settings (#653)
    * Adds a cache to game settings
    
    * Fix an error
    
    * More fixes to cache
    
    * clears cache before setting to force refressh
    
    * More fixes
    
    * update proxy IP, need to make this configurable
    
    * Make proxy forward better

--- DotNetTwitchBot/Program.cs
+++ DotNetTwitchBot/Program.cs
@@ -21,8 +21,9 @@ using Prometheus.DotNetRuntime;
 using Quartz;
 using Quartz.AspNetCore;
 using Serilog;
+using System.Collections;
 using System.Net;
-
+using System.Net.NetworkInformation;
 using TwitchLib.EventSub.Websockets.Extensions;
 internal class Program
 {
@@ -165,9 +166,12 @@ internal class Program
         builder.Services.Configure<ForwardedHeadersOptions>(options =>
         {
             options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-            options.KnownProxies.Add(IPAddress.Parse("192.168.1.128"));
             options.RequireHeaderSymmetry = false;
             options.ForwardLimit = null;
+            foreach(var network in GetNetworks(NetworkInterfaceType.Ethernet))
+            {
+                options.KnownNetworks.Add(network);
+            }
         });
 
         var app = builder.Build();
@@ -270,6 +274,53 @@ internal class Program
         else
         {
             Log.Fatal("Unhandled non-exception object:", e.ExceptionObject);
+        }
+    }
+
+    private static IEnumerable<Microsoft.AspNetCore.HttpOverrides.IPNetwork> GetNetworks(NetworkInterfaceType type)
+    {
+
+        foreach (var item in NetworkInterface.GetAllNetworkInterfaces()
+            .Where(n => n.NetworkInterfaceType == type && n.OperationalStatus == OperationalStatus.Up)  // get all operational networks of a given type
+            .Select(n => n.GetIPProperties())   // get the IPs
+            .Where(n => n.GatewayAddresses.Any())) // where the IPs have a gateway defined
+        {
+            var ipInfo = item.UnicastAddresses.FirstOrDefault(i => i.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork); // get the first cluster-facing IP address
+            if (ipInfo == null) { continue; }
+
+            // convert the mask to bits
+            var maskBytes = ipInfo.IPv4Mask.GetAddressBytes();
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(maskBytes);
+            }
+            var maskBits = new BitArray(maskBytes);
+
+            // count the number of "true" bits to get the CIDR mask
+            var cidrMask = maskBits.Cast<bool>().Count(b => b);
+
+            // convert my application's ip address to bits
+            var ipBytes = ipInfo.Address.GetAddressBytes();
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(ipBytes);
+            }
+            var ipBits = new BitArray(ipBytes);
+
+            // and the bits with the mask to get the start of the range
+            var maskedBits = ipBits.And(maskBits);
+
+            // Convert the masked IP back into an IP address
+            var maskedIpBytes = new byte[4];
+            maskedBits.CopyTo(maskedIpBytes, 0);
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(maskedIpBytes);
+            }
+            var rangeStartIp = new IPAddress(maskedIpBytes);
+
+            // return the start IP and CIDR mask
+            yield return new(rangeStartIp, cidrMask);
         }
     }
 }
