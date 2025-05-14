fe4d2bc394c21a1bd7cb33e1b4f0a57c2a81b3f5
SPTV <psycoboy@gmail.com>
2025-05-14 14:42:41 -0700

    Adds ability to change ticket amount

--- DotNetTwitchBot/Bot/Commands/Features/ITicketsFeature.cs
+++ DotNetTwitchBot/Bot/Commands/Features/ITicketsFeature.cs
@@ -4,6 +4,9 @@ namespace DotNetTwitchBot.Bot.Commands.Features
 {
     public interface ITicketsFeature
     {
+        Task<int> GetPointsForActiveUsers();
+        Task<int> GetPointsForEveryone();
+        Task<int> GetPointsForSubs();
         Task GiveTicketsToActiveAndSubsOnlineWithBonus(long amount, long bonusAmount);
         Task GiveTicketsToActiveUsers(long amount);
         Task<long> GiveTicketsToViewerByUserId(string userid, long amount);
@@ -11,5 +14,8 @@ namespace DotNetTwitchBot.Bot.Commands.Features
         Task GiveTicketsWithBonusToViewers(IEnumerable<string> viewers, long amount, long subBonusAmount);
         Task OnCommand(object? sender, CommandEventArgs e);
         Task Register();
+        Task SetPointsForActiveUsers(int points);
+        Task SetPointsForEveryone(int points);
+        Task SetPointsForSubs(int points);
     }
 }
