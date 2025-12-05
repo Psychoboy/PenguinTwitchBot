1677318166cd1c572c40776e98709f7a8a923a8a
SPTV <psycoboy@gmail.com>
2025-12-05 08:28:50 -0700

    Adds SC AI

---
+++ DotNetTwitchBot/Application/ChatMessage/Handlers/ReplyToMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using DotNetTwitchBot.Application.ChatMessage.Notifications;
+using DotNetTwitchBot.Bot.TwitchServices;
+using MediatR;
+
+namespace DotNetTwitchBot.Application.ChatMessage.Handlers
+{
+    public class ReplyToMessageHandler(ITwitchChatBot chatBot) : INotificationHandler<ReplyToMessage>
+    {
+        public Task Handle(ReplyToMessage request, CancellationToken cancellationToken)
+        {
+            return chatBot.ReplyToMessage(request.MessageId, request.Message);
+        }
+    }
+}
