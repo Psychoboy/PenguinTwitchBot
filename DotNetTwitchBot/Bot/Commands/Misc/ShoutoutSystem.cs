66536caa8bd74b640002f7a0712a4a16bb996c97
SPTV <psycoboy@gmail.com>
2025-12-06 09:32:38 -0700

    Remove duplicate check

--- DotNetTwitchBot/Bot/Commands/Misc/ShoutoutSystem.cs
+++ DotNetTwitchBot/Bot/Commands/Misc/ShoutoutSystem.cs
@@ -97,15 +97,12 @@ namespace DotNetTwitchBot.Bot.Commands.Misc
             if(autoShoutout != null && autoShoutout.UseAi && useAi)
             {
                 await using var scope = _scopeFactory.CreateAsyncScope();
-                if (userId != null)
+                var streamTitle = await _twitchService.GetUserStreamTitle(userId);
+                var bio = await _twitchService.GetUserBio(userId);
+                var shoutoutAi = scope.ServiceProvider.GetService<IShoutoutAi>();
+                if (shoutoutAi != null)
                 {
-                    var streamTitle = await _twitchService.GetUserStreamTitle(userId);
-                    var bio = await _twitchService.GetUserBio(userId);
-                    var shoutoutAi = scope.ServiceProvider.GetService<IShoutoutAi>();
-                    if (shoutoutAi != null)
-                    {
-                        message = await shoutoutAi.GetShoutoutForStreamer(name, game ?? "Unknown Game", streamTitle ?? "No Title", bio ?? "No Bio", autoShoutout.AdditionalPrompt);
-                    }
+                    message = await shoutoutAi.GetShoutoutForStreamer(name, game ?? "Unknown Game", streamTitle ?? "No Title", bio ?? "No Bio", autoShoutout.AdditionalPrompt);
                 }
             }
 
