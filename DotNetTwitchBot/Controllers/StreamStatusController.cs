5c5415cfe03c425887538efe579c259463b4a5af
Psychoboy <psycoboy@gmail.com>
2026-01-03 08:19:54 -0700

    Add if stream is online check

---
+++ DotNetTwitchBot/Controllers/StreamStatusController.cs
@@ -0,0 +1,22 @@
+﻿using DotNetTwitchBot.Bot.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTwitchBot.Controllers
+{
+    [ApiController]
+    public class StreamStatusController(IServiceBackbone serviceBackbone) : Controller
+    {
+        [HttpGet("/streamstatus")]
+        public async Task<ActionResult> GetStreamStatus()
+        {
+            if (serviceBackbone.IsOnline)
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Ok(false);
+            }
+        }
+    }
+}
