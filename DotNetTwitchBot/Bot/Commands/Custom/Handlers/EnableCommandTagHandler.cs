a21279c4944d2a3abe81811bef78a6623c4d4000
Psychoboy <psycoboy@gmail.com>
2026-04-04 11:56:53 -0700

    Updating the current handler to change update new stuff

--- DotNetTwitchBot/Bot/Commands/Custom/Handlers/EnableCommandTagHandler.cs
+++ DotNetTwitchBot/Bot/Commands/Custom/Handlers/EnableCommandTagHandler.cs
@@ -1,9 +1,10 @@
-﻿using DotNetTwitchBot.Bot.Commands.Custom.Tags;
+﻿using DotNetTwitchBot.Bot.Actions;
+using DotNetTwitchBot.Bot.Commands.Custom.Tags;
 using MediatR;
 
 namespace DotNetTwitchBot.Bot.Commands.Custom.Handlers
 {
-    public class EnableCommandTagHandler : IRequestHandler<EnableCommandTag, CustomCommandResult>
+    public class EnableCommandTagHandler(IServiceScopeFactory serviceScopeFactory, ILogger<EnableCommandTagHandler> logger) : IRequestHandler<EnableCommandTag, CustomCommandResult>
     {
         public async Task<CustomCommandResult> Handle(EnableCommandTag request, CancellationToken cancellationToken)
         {
@@ -14,6 +15,23 @@ namespace DotNetTwitchBot.Bot.Commands.Custom.Handlers
                 command.Disabled = false;
                 await request.CustomCommand.SaveCommand(command);
             }
+
+            await using var scope = serviceScopeFactory.CreateAsyncScope();
+            var actionManagement = scope.ServiceProvider.GetRequiredService<IActionManagementService>();
+            var actionService = scope.ServiceProvider.GetRequiredService<IAction>();
+            var actionCommandService = scope.ServiceProvider.GetRequiredService<IActionCommandService>();
+
+            var actionCommands = await actionCommandService.GetAllAsync();
+            var actionCommand = actionCommands.FirstOrDefault(c =>
+                c.CommandName.Equals(request.Args.Trim(), StringComparison.OrdinalIgnoreCase));
+
+            if (actionCommand != null)
+            {
+                actionCommand.Disabled = false;
+                await actionCommandService.UpdateAsync(actionCommand);
+                logger.LogInformation("Action command '{CommandName}' has been enabled.", actionCommand.CommandName);
+            }
+
             return new CustomCommandResult();
         }
     }
