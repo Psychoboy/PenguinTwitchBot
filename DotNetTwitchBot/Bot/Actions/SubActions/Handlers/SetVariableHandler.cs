166fb75f4e13e2b85768a3e660428ba4dc4c67ed
Psychoboy <psycoboy@gmail.com>
2026-04-06 12:37:23 -0700

    Set Variable works now

---
+++ DotNetTwitchBot/Bot/Actions/SubActions/Handlers/SetVariableHandler.cs
@@ -0,0 +1,23 @@
+﻿using DotNetTwitchBot.Bot.Actions.SubActions.Types;
+
+namespace DotNetTwitchBot.Bot.Actions.SubActions.Handlers
+{
+    public class SetVariableHandler(ILogger<SetVariableHandler> logger) : ISubActionHandler
+    {
+        public SubActionTypes SupportedType => SubActionTypes.SetVariable;
+
+        public Task ExecuteAsync(SubActionType subAction, Dictionary<string, string> variables)
+        {
+            if(subAction is not SetVariableType setVariableType)
+            {
+                logger.LogError("Invalid sub action type for SetVariableHandler: {Type}", subAction.GetType().Name);
+                return Task.CompletedTask;
+            }
+
+            setVariableType.Value = VariableReplacer.ReplaceVariables(setVariableType.Value, variables);
+            variables[$"{setVariableType.Text}"] = setVariableType.Value;
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- DotNetTwitchBot/Bot/Actions/SubActions/VariableReplacer.cs
+++ DotNetTwitchBot/Bot/Actions/SubActions/VariableReplacer.cs
@@ -1,14 +1,45 @@
+using System.Text.RegularExpressions;
+using org.mariuszgromada.math.mxparser;
+
 namespace DotNetTwitchBot.Bot.Actions.SubActions
 {
     public static class VariableReplacer
     {
+        private static bool CalledLicense = false;
         public static string ReplaceVariables(string input, Dictionary<string, string> variables)
         {
             foreach (var variable in variables)
             {
                 input = input.Replace($"%{variable.Key}%", variable.Value, StringComparison.OrdinalIgnoreCase);
             }
-            return input;
+            var result = Regex.Replace(input, @"\$math\((.+?)\)\$", match =>
+            {
+                var expression = match.Groups[1].Value;
+                // Do something with expression
+                return DoMath(expression);
+            });
+            return result;
+        }
+
+        private static string DoMath(string expression)
+        {
+            if(!CalledLicense)
+            {
+                License.iConfirmNonCommercialUse("SuperPenguinTV");
+                CalledLicense = true;
+            }
+
+
+            try
+            {
+               var e = new Expression(expression);
+                var result = e.calculate();
+                return result.ToString();
+            }
+            catch
+            {
+                return expression; // If it's not a valid expression, return the original input
+            }
         }
     }
 }
