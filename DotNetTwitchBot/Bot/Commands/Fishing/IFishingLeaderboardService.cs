6533465b3c9299f5b95971daef3cab41f2c450c5
Psychoboy <psycoboy@gmail.com>
2026-05-09 08:40:41 -0700

    Adds wall of shame

--- DotNetTwitchBot/Bot/Commands/Fishing/IFishingLeaderboardService.cs
+++ DotNetTwitchBot/Bot/Commands/Fishing/IFishingLeaderboardService.cs
@@ -6,6 +6,7 @@ namespace DotNetTwitchBot.Bot.Commands.Fishing
     public interface IFishingLeaderboardService
     {
         Task<List<LeaderPosition>> GetTotalGoldLeaderboard(int count = 50);
+        Task<List<SnapLossLeaderboardPosition>> GetSnapLossLeaderboard(int count = 50);
         Task<List<FishCatch>> GetMostValuableCatchesLeaderboard(int count = 50);
         Task<List<FishCatch>> GetRecentCatches(int count = 50);
         Task<List<FishCatch>> GetUserRecentCatches(string userId, int count = 50);
