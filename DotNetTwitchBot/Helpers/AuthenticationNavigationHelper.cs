6ab3d931aa103e41fd567e68652100e381eac22b
Psychoboy <psycoboy@gmail.com>
2026-05-09 14:39:57 -0700

    Adds better user experience when not signed in (#916)
    * Adds better user experience when not signed in
    
    * Fixed signin redirects

---
+++ DotNetTwitchBot/Helpers/AuthenticationNavigationHelper.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace DotNetTwitchBot.Helpers;
+
+public static class AuthenticationNavigationHelper
+{
+    public static void NavigateToSigninWithReturnUrl(this NavigationManager navigation, bool forceLoad = true)
+    {
+        var currentUri = new Uri(navigation.Uri);
+        var returnPath = string.IsNullOrEmpty(currentUri.PathAndQuery) ? "/" : currentUri.PathAndQuery;
+        if (!string.IsNullOrEmpty(currentUri.Fragment))
+        {
+            returnPath += currentUri.Fragment;
+        }
+
+        var signinUrl = QueryHelpers.AddQueryString("/signin", "r", returnPath);
+        navigation.NavigateTo(signinUrl, forceLoad);
+    }
+}
