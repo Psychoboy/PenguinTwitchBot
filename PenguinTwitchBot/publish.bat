18cc98d2a19deb5db230b35e2b73bf59d4b33ae4
Psychoboy <psycoboy@gmail.com>
2026-07-03 07:48:38 -0700

    Cleanup old files

--- PenguinTwitchBot/publish.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p VERSION=Enter version number (e.g. 1.2.3): 
-
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:Version=%VERSION% -p:InformationalVersion=%VERSION%
