56ab8b57537220e2fa384c4c8f5197fc2b0c4bf3
Psychoboy <psycoboy@gmail.com>
2026-07-07 16:29:23 -0700

    Update the triggers and subactions for tournaments

--- PenguinTwitchBot.Database/Bot/Actions/SubActions/Types/FishingTournamentStartType.cs
+++ PenguinTwitchBot.Database/Bot/Actions/SubActions/Types/FishingTournamentStartType.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.DependencyInjection;
 using PenguinTwitchBot.Database.Bot.Actions.SubActions.UI;
-using PenguinTwitchBot.Database.Bot.Models.Fishing;
 
 namespace PenguinTwitchBot.Database.Bot.Actions.SubActions.Types
 {
@@ -22,8 +20,6 @@ namespace PenguinTwitchBot.Database.Bot.Actions.SubActions.Types
 
         public List<SubActionUIField> GetUIFields(IServiceProvider? serviceProvider = null)
         {
-            var tournamentOptions = GetTournamentOptions(serviceProvider);
-
             return [
                 new()
                 {
@@ -37,11 +33,10 @@ namespace PenguinTwitchBot.Database.Bot.Actions.SubActions.Types
                 {
                     PropertyName = nameof(TournamentId),
                     Label = "Tournament / Template",
-                    FieldType = tournamentOptions.Count > 0 ? UIFieldType.Select : UIFieldType.Number,
+                    FieldType = UIFieldType.Number,
                     Required = true,
                     Min = 1,
-                    HelperText = "Select an existing tournament to start, or a template to clone and start.",
-                    SelectOptions = tournamentOptions.Count > 0 ? tournamentOptions : null
+                    HelperText = "Select an existing tournament to start, or a template to clone and start."
                 },
                 new()
                 {
@@ -93,58 +88,5 @@ namespace PenguinTwitchBot.Database.Bot.Actions.SubActions.Types
             return null;
         }
 
-        private static List<SelectOption> GetTournamentOptions(IServiceProvider? serviceProvider)
-        {
-            if (serviceProvider == null)
-            {
-                return [];
-            }
-
-            try
-            {
-                using var scope = serviceProvider.CreateScope();
-                var fishingServiceType = Type.GetType("PenguinTwitchBot.Bot.Commands.Fishing.IFishingService, PenguinTwitchBot");
-                if (fishingServiceType == null)
-                {
-                    return [];
-                }
-
-                var fishingService = scope.ServiceProvider.GetService(fishingServiceType);
-                if (fishingService == null)
-                {
-                    return [];
-                }
-
-                var getAllFishingTournaments = fishingServiceType.GetMethod("GetAllFishingTournaments", Type.EmptyTypes);
-                if (getAllFishingTournaments == null)
-                {
-                    return [];
-                }
-
-                var task = getAllFishingTournaments.Invoke(fishingService, []) as Task;
-                if (task == null)
-                {
-                    return [];
-                }
-
-                task.GetAwaiter().GetResult();
-                var result = task.GetType().GetProperty("Result")?.GetValue(task) as IEnumerable<FishingTournament>;
-                var tournaments = result?.ToList() ?? [];
-
-                return tournaments
-                    .OrderByDescending(tournament => tournament.StartsAtUtc)
-                    .ThenBy(tournament => tournament.Name)
-                    .Select(tournament => new SelectOption
-                    {
-                        Id = tournament.Id,
-                        Name = $"{tournament.Name} ({tournament.Status})"
-                    })
-                    .ToList();
-            }
-            catch
-            {
-                return [];
-            }
-        }
     }
 }
